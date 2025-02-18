26876fc51654c0db623016711056d8d80cf8929a
Jan Filip Dabrowski <datascienceworkjan@gmail.com>
2025-02-18 22:22:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST", "46.227.152.16")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "rededit_db")
 DB_USER = os.getenv("DB_USER", "postgres")
